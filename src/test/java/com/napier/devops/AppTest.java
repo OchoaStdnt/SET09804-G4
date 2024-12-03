ec3c73dd9dac0254e9e901e428fe789f66ca6cd9
Angel Ochoa <40688673@live.napier.ac.uk>
2024-12-02 23:23:54 -0600

    MOre test fields
    Signed-off-by: Ochoa-88 <157509216+Ochoa-88@users.noreply.github.com>

--- src/test/java/com/napier/devops/AppTest.java
+++ src/test/java/com/napier/devops/AppTest.java
@@ -22,10 +22,138 @@ public class AppTest
         method = new Methods();
     }
 
+    // Added dummy tests for the 32 methods
     @Test
-    void testShowWorldDBTables(){
+    void test1(){
         assertEquals(5, 5);
     }
+    @Test
+    void test2(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test3(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test4(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test5(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test6(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test7(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test8(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test9(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test10(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test11(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test12(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test13(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test14(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test15(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test16(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test17(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test18(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test19(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test20(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test21(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test22(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test23(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test24(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test25(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test26(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test27(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test28(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test29(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test30(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test31(){
+        assertEquals(5, 5);
+    }
+    @Test
+    void test32(){
+        assertEquals(5, 5);
+    }
+
+
+
 
     /*
     @Test
